6b26d831766773934fe4ba96f162026acf8ca492
jim3 <j3.casey@protonmail.com>
2022-09-19 14:35:57 -0400

    add files

---
+++ blockchair.js
@@ -0,0 +1,45 @@
+const API = "https://api.blockchair.com/bitcoin/stats";
+
+window.addEventListener("load", (event) => {
+  event.preventDefault();
+  getMarketPrice();
+});
+
+function getMarketPrice() {
+  fetch(API)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const marketPrice = document.querySelector("#market_price_usd");
+      marketPrice.innerHTML = currencyFormatter(`${data.data.market_price_usd}`);
+
+      const marketPriceChange = document.querySelector("#market_price_usd_change_24h_percentage");
+      marketPriceChange.innerHTML = percentageFormatter(`${data.data.market_price_usd_change_24h_percentage / 100}`);
+
+      const avgTransactionFee = document.querySelector("#average_transaction_fee_usd_24h");
+      avgTransactionFee.innerHTML = currencyFormatter(`${data.data.average_transaction_fee_usd_24h}`);
+
+      const marketDominance = document.querySelector("#market_dominance_percentage");
+      marketDominance.innerHTML = percentageFormatter(`${data.data.market_dominance_percentage / 100}`);
+
+      const circulation = document.querySelector("#circulation");
+      circulation.innerHTML = currencyFormatter(`${data.data.circulation}`);
+
+      setTimeout(getMarketPrice, 60000);
+    });
+}
+
+function currencyFormatter(number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number);
+}
+
+function percentageFormatter(value) {
+  return new Intl.NumberFormat("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  }).format(value);
+}
