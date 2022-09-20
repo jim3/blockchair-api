fdabb0bc11aca70f939053f59ef5039f8c6cbc0f
jim3 <j3.casey@protonmail.com>
2022-09-20 15:41:32 -0400

    Update blockchair.js

--- blockchair.js
+++ blockchair.js
@@ -6,31 +6,32 @@ window.addEventListener("load", (event) => {
 });
 
 function getMarketPrice() {
-  try{
-  fetch(API)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const marketPrice = document.querySelector("#market_price_usd");
-      marketPrice.innerHTML = currencyFormatter(`${data.data.market_price_usd}`);
+  try {
+    fetch(API)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const marketPrice = document.querySelector("#market_price_usd");
+        marketPrice.innerHTML = currencyFormatter(`${data.data.market_price_usd}`);
 
-      const marketPriceChange = document.querySelector("#market_price_usd_change_24h_percentage");
-      marketPriceChange.innerHTML = percentageFormatter(`${data.data.market_price_usd_change_24h_percentage / 100}`);
+        const marketPriceChange = document.querySelector("#market_price_usd_change_24h_percentage");
+        marketPriceChange.innerHTML = percentageFormatter(`${data.data.market_price_usd_change_24h_percentage / 100}`);
 
-      const avgTransactionFee = document.querySelector("#average_transaction_fee_usd_24h");
-      avgTransactionFee.innerHTML = currencyFormatter(`${data.data.average_transaction_fee_usd_24h}`);
+        const avgTransactionFee = document.querySelector("#average_transaction_fee_usd_24h");
+        avgTransactionFee.innerHTML = currencyFormatter(`${data.data.average_transaction_fee_usd_24h}`);
 
-      const marketDominance = document.querySelector("#market_dominance_percentage");
-      marketDominance.innerHTML = percentageFormatter(`${data.data.market_dominance_percentage / 100}`);
+        const marketDominance = document.querySelector("#market_dominance_percentage");
+        marketDominance.innerHTML = percentageFormatter(`${data.data.market_dominance_percentage / 100}`);
 
-      const circulation = document.querySelector("#circulation");
-      circulation.innerHTML = currencyFormatter(`${data.data.circulation}`);
+        const circulation = document.querySelector("#circulation");
+        circulation.innerHTML = currencyFormatter(`${data.data.circulation}`);
 
-      setTimeout(getMarketPrice, 60000);
-    });
+        setTimeout(getMarketPrice, 60000);
+      });
+  } catch (error) {
+    console.error(`Could not fetch data: ${error}`);
   }
-    catch (error) {console.error(`Could not fetch data: ${error}`);}
 }
 
 function currencyFormatter(number) {
